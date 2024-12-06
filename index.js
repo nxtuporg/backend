d97d62b08028877f6fd641bed09a46a6630f7643
Aditya kumar <adityakr80510@gmail.com>
2024-12-07 02:15:13 +0530

    update

--- index.js
+++ index.js
@@ -1,49 +1,42 @@
-const express = require('express');
-const dotenv = require("dotenv").config()
+const express = require("express");
+const dotenv = require("dotenv").config();
 const connectDB = require("./config/db.js");
 const app = express();
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocs = require('./config/swaggerOptions.js');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocs = require("./config/swaggerOptions.js");
 connectDB();
 
-app.use(express.json()); 
+app.use(express.json());
 
+const cors = require("cors");
+app.use(cors());
+const PORT = process.env.PORT || 6969;
 
-const cors = require('cors');
-app.use(cors());            
-const PORT = process.env.PORT || 6969
+app.get("/", (req, res) => {
+  res.json("working");
+});
 
-app.get('/',(req,res)=>{
-    res.json("working")
-})
- 
-
-const eventsRoutes = require('./src/routes/eventsRoutes.js')
-const clanRoutes = require('./src/routes/clanRoutes.js')
-const clubRoutes = require('./src/routes/clubRoutes.js')
-const carouselRoutes = require('./src/routes/carouselRoutes.js')
+const eventsRoutes = require("./src/routes/eventsRoutes.js");
+const clanRoutes = require("./src/routes/clanRoutes.js");
+const clubRoutes = require("./src/routes/clubRoutes.js");
+const carouselRoutes = require("./src/routes/carouselRoutes.js");
 const authRoutes = require("./src/routes/authRoutes.js");
-const imageRoutes = require("./src/routes/imageRoute.js")
+const imageRoutes = require("./src/routes/imageRoute.js");
+const registrationRoutes = require("./src/routes/registrations.js");
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use('/api/events',eventsRoutes)
-app.use('/api/clans',clanRoutes)
-app.use('/api/clubs',clubRoutes)
-app.use('/api/carousel',carouselRoutes)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api/events", eventsRoutes);
+app.use("/api/clans", clanRoutes);
+app.use("/api/clubs", clubRoutes);
+app.use("/api/carousel", carouselRoutes);
 app.use("/api/user", authRoutes);
 app.use("/api/uploadImage", imageRoutes);
 app.use("/api/registrations", registrationRoutes);
 
-
-
 app.use(express.json());
-
 
 module.exports = app;
 
-
-
-app.listen(PORT,()=>{
-    console.log(`running on port ${PORT}`)
-})
-
+app.listen(PORT, () => {
+  console.log(`running on port ${PORT}`);
+});
